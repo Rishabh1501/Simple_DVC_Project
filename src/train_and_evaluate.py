c14e1a1badfd1cccb57ff404356d93f502fd8867
Rishabh Kalra <rishabhkalra1501@gmail.com>
2021-03-27 19:55:11 +0530

    formatted according to pep8

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1,6 +1,6 @@
-#load train and test files
-#train algorithms
-#save the metrics , params
+# load train and test files
+# train algorithms
+# save the metrics , params
 
 import os
 import warnings
@@ -22,6 +22,7 @@ def eval_metrics(actual, pred):
     r2 = r2_score(actual, pred)
     return rmse, mae, r2
 
+
 def train_and_evaluate(config_path):
     config = read_params(config_path)
     train_data_path = config['split_data']['train_path']
@@ -34,14 +35,14 @@ def train_and_evaluate(config_path):
     l1_ratio = config['estimators']['ElasticNet']['params']['l1_ratio']
     target = config['base']['target_col']
 
-    train = pd.read_csv(train_data_path,sep=',')
-    test = pd.read_csv(test_data_path,sep=',')
+    train = pd.read_csv(train_data_path, sep=',')
+    test = pd.read_csv(test_data_path, sep=',')
 
     train_y = train[target]
     test_y = test[target]
 
     train_x = train.drop(target, axis=1)
-    test_x = test.drop(target,axis=1)
+    test_x = test.drop(target, axis=1)
 
     lr = ElasticNet(
         alpha=alpha,
@@ -58,8 +59,7 @@ def train_and_evaluate(config_path):
     print("  MAE: %s" % mae)
     print("  R2: %s" % r2)
 
-
-#####################################################
+    #####################################################
     scores_file = config["reports"]["scores"]
     params_file = config["reports"]["params"]
 
@@ -77,8 +77,7 @@ def train_and_evaluate(config_path):
             "l1_ratio": l1_ratio,
         }
         json.dump(params, f, indent=4)
-#####################################################
-
+    #####################################################
 
     os.makedirs(model_dir, exist_ok=True)
     model_path = os.path.join(model_dir, "model.joblib")
@@ -86,8 +85,7 @@ def train_and_evaluate(config_path):
     joblib.dump(lr, model_path)
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 class NotInRange(Exception):
     def __init__(self, message="value not in range"):
         self.message = message
